bcdb07903a1e3e723d31f5d7cebeba91132df222
Ruslan <rv.volovik88@gmail.com>
2021-03-21 15:38:14 +0200

    feat: add constants for auth

---
+++ src/modules/auth/auth.constants.ts
@@ -0,0 +1,4 @@
+export const SUCCESS = 'success';
+export const SEND_CONFIRMATION_CODE = 'success';
+export const TEST_VALID_CONFIRMATION_CODE = '1111';
+export const REGEXP_PHONE_NUMBER = /^[0-9\-\+]{8,13}$/;
